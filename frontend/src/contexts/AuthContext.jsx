57ba327054a7f07480d5e42d5b27f501044a409a
Ilham Fajar Andika <ilhamfajar317@gmail.com>
2025-11-07 22:40:06 +0700

    Ubah nama services/authApi

--- frontend/src/contexts/AuthContext.jsx
+++ frontend/src/contexts/AuthContext.jsx
@@ -1,5 +1,5 @@
 import { createContext, useContext, useState, useEffect } from "react";
-import { authAPI } from "../services/api";
+import { authAPI } from "../services/authApi";
 
 const AuthContext = createContext();
 
